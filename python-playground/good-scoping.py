05238a9ed5b942962edd4e68b0433dc9e8cfb055
Magnus Jonsson <jmagnusj@gmail.com>
2008-06-18 01:27:21 -0400

    Improved nuggests about Python's scoping.

--- python-playground/good-scoping.py
+++ python-playground/good-scoping.py
@@ -1,3 +1,5 @@
+# in this case, python does the right thing.
+
 def hello(x):
     def hi(x):
         print x
@@ -6,3 +8,16 @@ def hello(x):
     print x
 
 hello(1)
+
+print "---"
+
+# here too.
+
+def bye(x):
+    def cu(y):
+        x = y
+    print x
+    cu(x+1)
+    print x
+
+bye(1)
--- python-playground/weird-scoping.py
+++ python-playground/weird-scoping.py
@@ -1,18 +1,38 @@
 # be aware of python's strange scoping rules...
-# they made me lose hours bughunting -- magnus
+# they made me lose an hour hunting for a
+# subtle but simple bug -- magnus
 
 def test():
     while True:
         a = 5
         break
     # a is not gone!
-    # in a sane language a should not exist anymore.
+    # ...in a saner language a should not exist anymore.
     print a
 
-    for i in range(1,2):
+    for i in [1,2]:
         if i == 2:
             print b # b from last iteration still exists!
-                    # in a sane language it should not exist anymore.
+                    # ...in a saner language b should not exist anymore.
         b = i
 
 test()
+
+
+# another funny thing:
+
+def boo(x):
+    def baa():
+        print x   # this is okay
+    baa()
+
+boo(1)
+
+def moo():
+    x = 1
+    def maa():
+        x = x     # this is not okay
+    maa()
+
+
+moo()
