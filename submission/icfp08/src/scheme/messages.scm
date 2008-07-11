981042654f7560fe871f8735f240f325f3e6d48a
Magnus Jonsson <jmagnusj@gmail.com>
2008-07-11 16:12:25 -0400

    structures to contain server messages

---
+++ submission/icfp08/src/scheme/messages.scm
@@ -0,0 +1,32 @@
+#lang scheme
+
+(provide init telemetry vehicle object success failure)
+
+(define-struct init (dx dy ; meters
+                     time-limit ; seconds
+                     min-sensor max-sensor ; meters
+                     max-speed ; meters/second
+                     max-turn max-hard-turn ; degrees/second
+                     ))
+
+(define-struct telemetry (time-stamp ; seconds
+                          acceleration-control ; -1; braking, 0: rolling, 1: accelerating
+                          turn-control         ; -2: hard left, -1 left, 0 straight, 1 right, 2 hard right
+                          vehicle ; vehicle
+                          objects ; (list (or object vehicle))
+                          ))
+
+(define-struct vehicle (x y ; meters
+                        dir ; degrees
+                        speed ; meters/second
+                        ))
+
+(define-struct object (kind ; (or 'boulder 'crater 'home-base)
+                       x y ; meters
+                       radius ; meters
+                       ))
+
+(define-struct success ())
+
+(define-struct failure (reason ; (or 'crash 'killed 'disconnected)
+                    ))
