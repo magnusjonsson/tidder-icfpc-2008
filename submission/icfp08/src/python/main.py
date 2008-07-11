4b32766c538e3484114ddfe8f57a96520da93c6e
Florian Haeglsperger <florian@haeglsperger.com>
2008-07-11 23:09:26 +0200

    asynchronous read from google.com

--- submission/icfp08/src/python/main.py
+++ submission/icfp08/src/python/main.py
@@ -1,6 +1,32 @@
 #! /usr/bin/env python
 
 import socket
+from Queue import Queue
+from threading import Thread
+import time
+import sys
+
+read_queue = Queue(0)
+
+class ReaderThread(Thread):
+    def __init__(self, s):
+        Thread.__init__(self)
+        self.socket = s
+    
+    def run(self):
+        while True:
+            try:
+                d = self.socket.recv(1)
+            except socket.error, e:
+                break
+            if d:
+                assert len(d) == 1
+                c = d[0]
+                
+                read_queue.put(c)
+                
+                # debug
+                sys.stdout.write(c)
 
 if __name__ == "__main__":
     host = "google.com"
@@ -8,8 +34,11 @@ if __name__ == "__main__":
     
     s = socket.socket()
     s.connect((host, port))
-    s.
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     
-    # do work
+    ReaderThread(s).start()
+    
+    s.send("GET / HTTP/1.0\nHost: www.google.com\n\n")
+    time.sleep(4)
     
     s.close()
