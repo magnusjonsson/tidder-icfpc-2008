969340f30fb262090920062b793eb34b30266b57
Magnus Jonsson <jmagnusj@gmail.com>
2008-07-11 18:19:01 -0400

    Parser bugfixes

--- submission/icfp08/src/scheme/messages.scm
+++ submission/icfp08/src/scheme/messages.scm
@@ -7,28 +7,38 @@
                      min-sensor max-sensor ; meters
                      max-speed ; meters/second
                      max-turn max-hard-turn ; degrees/second
-                     ))
+                     )
+  #:transparent)
 
 (define-struct telemetry (time-stamp ; seconds
                           acceleration-control ; -1; braking, 0: rolling, 1: accelerating
                           turn-control         ; -2: hard left, -1 left, 0 straight, 1 right, 2 hard right
                           vehicle ; vehicle
                           seen ; (list (or object vehicle))
-                          ))
+                          )
+  #:transparent)
 
 (define-struct vehicle (x y ; meters
                         dir ; degrees
                         speed ; meters/second
-                        ))
+                        )
+  #:transparent)
 
 (define-struct object (kind ; (or 'boulder 'crater 'home-base)
                        x y ; meters
                        radius ; meters
-                       ))
+                       )
+  #:transparent)
 
-(define-struct success ())
+(define-struct success (time)
+  #:transparent)
 
-(define-struct failure (reason ; (or 'crash 'killed 'disconnected)
-                    ))
+(define-struct failure (time
+                        reason ; (or 'crash 'crater 'killed 'disconnected)
+                        )
+  #:transparent)
 
-(define-struct end ())
+(define-struct end (time
+                    score
+                    )
+  #:transparent)
