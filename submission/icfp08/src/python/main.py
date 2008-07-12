0fb917df86b0632f33f4a1ff727f6a6360d1cc75
Florian Haeglsperger <florian@haeglsperger.com>
2008-07-12 02:47:16 +0200

    comment in main.py

--- submission/icfp08/src/python/main.py
+++ submission/icfp08/src/python/main.py
@@ -8,6 +8,11 @@ from Connection import Connection
 from Controller import Controller
 from Brain import Brain
 
+# The app is driven by the read loop in Connection.process().
+# Characters are pushed into the Controller, which collects them
+# to messages, parses these message and pushes them into the Brain.
+# The Brain then updates its state and issues commands.
+
 if __name__ == "__main__":
     host = "127.0.0.1"
     port = 17676
