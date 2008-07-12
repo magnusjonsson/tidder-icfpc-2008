fbae83b35aa73297c8c3c8da207116dcc3c09189
Magnus Jonsson <jmagnusj@gmail.com>
2008-07-12 11:57:56 -0400

    Simplified steering, removed SSE crap.

--- submission/icfp08/src/scheme/parser-test.scm
+++ submission/icfp08/src/scheme/parser-test.scm
@@ -4,14 +4,8 @@
 (require "network.scm")
 (require "messages.scm")
 (require "ai.scm")
-(require "path.scm")
-(require "remember.scm")
 
-; hack to break circular dependency;
-; not yet sure how to structure this properly
-(set-update-path! update-path)
-
-(connect-server "127.0.0.1" 17676)
+(connect-server "192.168.0.4" 17676)
 (do () (#f)
   (when (message-available?)
     (handle-message (get-message)))
