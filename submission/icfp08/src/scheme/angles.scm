e3c4205ecdc72ffb00daaecef081058cac783195
Florian Haeglsperger <florian@haeglsperger.com>
2008-07-12 12:12:34 +0200

    helper function atan-deg

--- submission/icfp08/src/scheme/angles.scm
+++ submission/icfp08/src/scheme/angles.scm
@@ -1,6 +1,6 @@
 #lang scheme
 
-(provide rad->deg deg- normalize-deg unwrap-deg)
+(provide rad->deg deg- normalize-deg unwrap-deg atan-deg)
 
 (define (rad->deg rad)
   (* 180 (/ rad pi)))
@@ -14,3 +14,5 @@
 
 (define (unwrap-deg reference deg)
   (+ reference (deg- deg reference)))
+
+(define (atan-deg x y) (rad->deg (atan (- 0 y) (- 0 x))))
