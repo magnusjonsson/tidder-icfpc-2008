24a234854f2c27d2b0967276fc6a143699992d8f
Magnus Jonsson <jmagnusj@gmail.com>
2008-07-12 13:59:18 -0400

    Tangent computation without the trig functions.

--- submission/icfp08/src/scheme/tangent.scm
+++ submission/icfp08/src/scheme/tangent.scm
@@ -2,7 +2,7 @@
 
 (require "angles.scm")
 
-(provide tangent)
+(provide tangent tangent2)
 
 ; returns (values x y angle distance) for the tangent point
 ; direction is -1 for right, 1 for left
@@ -14,6 +14,21 @@
          (alpha2 (* direction alpha))
          (tangent-angle (+ (atan dy dx) alpha2))
          (tangent-distance (sqrt (- (* d d) (* r r))))
-         (tangent-x (+ (* (cos tangent-angle) tangent-distance) robot-x))
-         (tangent-y (+ (* (sin tangent-angle) tangent-distance) robot-y)))
+         (tangent-x (* (cos tangent-angle) tangent-distance))
+         (tangent-y (* (sin tangent-angle) tangent-distance)))
     (values tangent-x tangent-y (rad->deg tangent-angle) tangent-distance)))
+
+(define (tangent2 robot-x robot-y circle-x circle-y r direction)
+  (let* ((dx (- circle-x robot-x))
+         (dy (- circle-y robot-y))
+         (d2 (+ (* dx dx) (* dy dy)))
+         (d  (sqrt d2)) ; avoid producing imaginary numbers
+         (r (min d r)) ; reduce radius if we're inside the radius already
+         (r2 (* r r))
+         (t2 (- d2 r2))
+         (t  (sqrt (abs t2))) ; avoid producing imaginary numbers
+         (cos (/ t d))
+         (sin (/ r d))
+         (tx (+ robot-x (* cos (+ (* dx cos) (* -1 dy direction sin)))))
+         (ty (+ robot-y (* cos (+ (* dy cos) (* dx direction sin) )))))
+    (values tx ty #f t)))
