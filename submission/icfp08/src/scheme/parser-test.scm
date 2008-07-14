68fbbd91aad13b6aa7010733db4c4732ea52a44b
rer0tsaz <i@s.invalid>
2008-07-14 04:27:59 +0200

    Disable exception catching while debugging

--- submission/icfp08/src/scheme/parser-test.scm
+++ submission/icfp08/src/scheme/parser-test.scm
@@ -5,17 +5,14 @@
 (require "messages.scm")
 (require "ai.scm")
 
-
-
-(define (do-it server port)
+(define (do-it server port debug)
   (connect-server server port)
   (do () (#f)
     (let/ec escape
-      (with-handlers* ((exn:fail?
+      (with-handlers* (((lambda (x) (and (not debug) (exn:fail? x)))
                         (lambda (v)
                           (printf "Catched ~a, trying to continue...~n" v)
                           (escape (void)))))
-                      
                       (when (message-available?)
                         (handle-message (get-message))))
       (sleep)))
@@ -23,4 +20,5 @@
 
 (when (>= (vector-length (current-command-line-arguments)) 2)
   (do-it (vector-ref (current-command-line-arguments) 0)
-         (string->number (vector-ref (current-command-line-arguments) 1))))
+         (string->number (vector-ref (current-command-line-arguments) 1))
+         #f))
