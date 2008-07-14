b2d98d4ed62efb09eb5133340fb0962c2a4ef62b
rer0tsaz <i@s.invalid>
2008-07-14 20:01:31 +0200

    Tweak constants

--- submission/icfp08/src/scheme/ai.scm
+++ submission/icfp08/src/scheme/ai.scm
@@ -29,8 +29,8 @@
 
 ;Martian avoidance parameters
 (define viewing-width (/ 60 2))
-(define viewing-distance 30)
-(define move-away-strength-1/ 3)
+(define viewing-distance 40)
+(define move-away-strength-1/ 1.5)
 
 ; whether to compute something on inbox-empty
 (define current-telemetry #f)
@@ -63,7 +63,7 @@
      (control-clear)
      (must-recompute-path))
     ((telemetry? m)
-
+     
      (remember-objects (map preprocess-object (telemetry-seen m)))
      
      (when (remembered-dirty?)
@@ -80,90 +80,89 @@
        
        (speedometer-update t pos)
        (turnometer-update t dir)))
-     
+    
     ((inbox-empty? m)
      (when current-telemetry
        (set! m current-telemetry)
        (set! current-telemetry #f)
-
-     (let* ((t (telemetry-time m))
-            (self (telemetry-vehicle m))
-            (pos (vehicle-pos self))
-            (dir (vehicle-dir self))
-            (speed (vehicle-speed self))
-            (target (compute-target pos)))
        
-       (printf "~n")
-       (printf "target: ~a ~n" target)
-       
-       (let* ((relative (curryr vec2- pos))
-              (target-rel (relative target))
-              (target-dir (vec2-angle-deg target-rel))
-              (martians-rel
-               (map (compose relative vehicle-pos)
-                    (filter vehicle? (telemetry-seen m))))
-              (ahead? (lambda (x) (< (abs (deg- target-dir x)) viewing-width)))
-              (martians-close
-               (filter (compose (curryr < viewing-distance) vec2-length) martians-rel))
-              (martians-ahead
-               (filter (compose ahead? vec2-angle-deg) martians-close))
-              (martian-mean-dir
-               (and (not (null? martians-ahead))
-                    (vec2-angle-deg
-                     (apply vec2+ (map vec2-normalize martians-ahead)))))
-              (adjusted-dir
-               (if martian-mean-dir
-                   (vec2-angle-deg
-                    (vec2+ (vec2-scale (sub1 move-away-strength-1/)
-                                       (vec2-normalize target-rel))
-                           (angle-deg->vec2
-                            (- target-dir (- (- target-dir martian-mean-dir))))))
-                   target-dir))
-              
-              ; search forwards to see at what point we will crash going forwards, if any.
-              (crash-distance (first-hit-time pos (angle-deg->vec2 dir)))
-              (dir-target-diff (deg- adjusted-dir dir))
-              (steer (* 2 dir-target-diff))
-              (wanted-speed (min
-                             ; drive slower when trying to turn
-                             (/ 320.0 (max 0.001 (abs steer)))
-                             ; drive slower when rotating
-                             (/ 320.0 (max 0.001 (abs (turnometer-value))))
-                             ; try to be able to stop in time if something shows up ahead.
-                             (* 2.0 (sqrt (max 0 (- (min (or crash-distance +inf.0)
-                                                         max-sensor)
-                                                    safety-margin))))
-                             ))
-              (speed (speedometer-value))
-              
-              (accel (sgn (- wanted-speed speed))))
+       (let* ((t (telemetry-time m))
+              (self (telemetry-vehicle m))
+              (pos (vehicle-pos self))
+              (dir (vehicle-dir self))
+              (speed (vehicle-speed self))
+              (target (compute-target pos)))
          
-         ;(printf "crash-distance: ~a~n" crash-distance)
-         ;(printf "accel: ~a steer: ~a  speed: ~a wanted-speed: ~a~n" accel steer speed wanted-speed)
-         ;(unless (> .001 (abs (- target-dir adjusted-dir)))
-         ;  (printf "Original direction: ~a~nAdjusted:~a~n" target-dir adjusted-dir))
-         ;(printf "target-distance: ~a~n" target-distance)
-         (control-set-state-deg/sec accel steer)
+         (printf "~n")
+         (printf "target: ~a ~n" target)
          
-         (when (gfx-on?)
-           ; background
-           (gfx-color 255 255 255)
-           (gfx-clear)
-           ; draw remembered
-           (gfx-color 255 0 0)
-           (draw-remembered)
-           ; draw home base
-           (gfx-color 0 255 0)
-           (gfx-circle 0 0 5)
-           ; draw A* path
-           (gfx-color 128 128 128)
-           (draw-path pos)
-           ; draw line to target
-           (gfx-color 128 128 255)
-           (gfx-line (vec2-x pos) (vec2-y pos) (vec2-x target) (vec2-y target))
-           ; draw rover
-           (gfx-color 0 0 255)
-           (gfx-circle (vec2-x pos) (vec2-y pos) 5)
-           ; show it
-           (gfx-show)
-           )))))))
+         (let* ((relative (curryr vec2- pos))
+                (target-rel (relative target))
+                (target-dir (vec2-angle-deg target-rel))
+                (martians-rel
+                 (map (compose relative vehicle-pos)
+                      (filter vehicle? (telemetry-seen m))))
+                (ahead? (lambda (x) (< (abs (deg- target-dir x)) viewing-width)))
+                (martians-close
+                 (filter (compose (curryr < viewing-distance) vec2-length) martians-rel))
+                (martians-ahead
+                 (filter (compose ahead? vec2-angle-deg) martians-close))
+                (martian-mean-dir
+                 (and (not (null? martians-ahead))
+                      (vec2-angle-deg
+                       (apply vec2+ (map vec2-normalize martians-ahead)))))
+                (adjusted-dir
+                 (if martian-mean-dir
+                     (vec2-angle-deg
+                      (vec2+ (vec2-scale (sub1 move-away-strength-1/)
+                                         (vec2-normalize target-rel))
+                             (angle-deg->vec2
+                              (- target-dir (- (- target-dir martian-mean-dir))))))
+                     target-dir))
+                
+                ; search forwards to see at what point we will crash going forwards, if any.
+                (crash-distance (first-hit-time pos (angle-deg->vec2 dir)))
+                (dir-target-diff (deg- adjusted-dir dir))
+                (steer (* 2 dir-target-diff))
+                (wanted-speed (min
+                               ; drive slower when trying to turn
+                               (/ 3000.0 (max 0.001 (abs steer)))
+                               ; drive slower when rotating
+                               (/ 3000.0 (max 0.001 (abs (turnometer-value))))
+                               ; try to be able to stop in time if something shows up ahead.
+                               (* 3.0 (sqrt (max 0 (- (or crash-distance +inf.0) safety-margin))))
+                               (* 4.0 (sqrt (max 0 (- max-sensor safety-margin))))
+                               ))
+                (speed (speedometer-value))
+                
+                (accel (sgn (- wanted-speed speed))))
+           
+           ;(printf "crash-distance: ~a~n" crash-distance)
+           (printf "accel: ~a steer: ~a  speed: ~a wanted-speed: ~a~n" accel steer speed wanted-speed)
+           ;(unless (> .001 (abs (- target-dir adjusted-dir)))
+           ;  (printf "Original direction: ~a~nAdjusted:~a~n" target-dir adjusted-dir))
+           ;(printf "target-distance: ~a~n" target-distance)
+           (control-set-state-deg/sec accel steer)
+           
+           (when (gfx-on?)
+             ; background
+             (gfx-color 255 255 255)
+             (gfx-clear)
+             ; draw remembered
+             (gfx-color 255 0 0)
+             (draw-remembered)
+             ; draw home base
+             (gfx-color 0 255 0)
+             (gfx-circle 0 0 5)
+             ; draw A* path
+             (gfx-color 128 128 128)
+             (draw-path pos)
+             ; draw line to target
+             (gfx-color 128 128 255)
+             (gfx-line (vec2-x pos) (vec2-y pos) (vec2-x target) (vec2-y target))
+             ; draw rover
+             (gfx-color 0 0 255)
+             (gfx-circle (vec2-x pos) (vec2-y pos) 5)
+             ; show it
+             (gfx-show)
+             )))))))
